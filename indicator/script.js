05d41c55cab67de5b9e3b653d6af48433016e496
marta.rondan <marta.rondan@alten.es>
2024-07-11 13:01:43 +0200

    program cards y funcionamiento de subtab

--- indicator/script.js
+++ indicator/script.js
@@ -4,6 +4,7 @@ const content = document.getElementsByClassName('content__option');
 // Siendo tabs un htmlcollection no se puede iterar como un array para ello necesitamos Array.from(tabs)
 Array.from(tabs).forEach((tab, i) => {
   tab.addEventListener('click', () => {
+    // debugger
     Array.from(tabs).forEach((tab, i) => {
       tab.classList.remove('active');
       content[i].classList.remove('active');
@@ -60,12 +61,31 @@ document.addEventListener("DOMContentLoaded", () => {
       createAircraftPlanned(cardsData.length, aircraftDelivered.length);
       createDateTable(dataAux);
       createProductionCards(cardsData, aircraftDelivered);
+      // createProgramsCards(cardsData, aircraftDelivered);
 
     })
     .catch(error => console.error("Error fetching data:", error));
 });
 
+// Trae los datos de programs_data.json y los adapta a programs
+document.addEventListener("DOMContentLoaded", () => {
 
+  fetch("programs_data.json")
+    .then(response => response.json())
+    .then(cardsData => {
+
+      const dataAux = [...cardsData];
+      const aircraftDelivered = cardsData.filter((card) => card?.completed);
+      cardsData = dataAux.filter((card) => !card?.completed);
+
+      // createAircraftPlanned(cardsData.length, aircraftDelivered.length);
+      // createDateTable(dataAux);
+      // createProductionCards(cardsData, aircraftDelivered);
+      createProgramsCards(cardsData, aircraftDelivered);
+
+    })
+    .catch(error => console.error("Error fetching data:", error));
+});
 
 const createAircraftPlanned = (noCompleted, completed) => {
   const aircraftPlanned = document.getElementById("aircraft_planned");
@@ -131,7 +151,7 @@ const createDateTable = (data) => {
   dateTable.appendChild(table);
 }
 
-
+// Metodo que crea las tarjetas de Production
 const createProductionCards = (cardsData, aircraftDelivered) => {
   const planeModal = document.getElementById('plane__modal');
   const modalContainer = document.getElementById('modal__container');
@@ -319,3 +339,153 @@ openExcel.addEventListener('click', () => {
   // let Hoja = Libro.Worksheets(1);
   // Hoja.Application.Visible = true;
 });
+
+// metodo para crear las tarjetas de los programas (a400m, f8x,f10x...)
+const createProgramsCards = (cardsData, aircraftDelivered) => {
+  const planeModal = document.getElementById('program__modal');
+  const modalContainer = document.getElementById('program_modal__container');
+
+  const cardSection = document.getElementById("program__list");
+
+  cardsData.forEach((data, i) => {
+
+    const card = document.createElement("div");
+    card.className = "a400m__card";
+    card.innerHTML = `<h3 class="a400m__station">${data.subprogram}</h3>`
+
+    const cardContent = document.createElement("div");
+    cardContent.className = "a400m__card_content";
+
+    const plane_graph = document.createElement("img");
+    plane_graph.className = "production__plane_graph";
+    plane_graph.src = "../assets/img/webp_img/plane_graph-removebg-preview.webp";
+    plane_graph.alt = "plane_image";
+
+
+    card.addEventListener('click', () => {
+
+      modalContainer.innerHTML = `
+                              <header>
+                                  <span class="modal__date">${data.cardDate}</span>
+                                  <span class="modal__title">${data.subprogram}</span>
+                                  <div class="modal__msn">
+                                      <img src="${data.flag}" class="modal__flag" alt="flag">
+                                      <span class="modal__n_msn">MSN ${data.msn}</span>
+                                  </div>
+                                  <i id="modal__close" class="fas fa-times"></i>
+                              </header>`
+
+      const modalClose = document.getElementById('modal__close');
+      modalClose.addEventListener('click', () => planeModal.classList.remove('modal--show'));
+
+      const main = document.createElement('main');
+      data?.delivery == false
+        ? main.className = 'modal__content'
+        : main.className = 'modal__content delivery';
+
+      main.innerHTML = `
+                  <div class="modal__start_end_date">
+                      <p>ST Start date</p>
+                      <span>${data.startDate}</span>
+                      <p>ST Finish Date</p>
+                      <span>${data.endDate}</span>
+                      <p>WD next station change</p>
+                      <span>${data.nextStation}</span>
+                  </div>
+                  `;
+
+      const modal1 = `
+                      <div>
+                          <div>
+                              <span>AC Progress - Station</span>
+                              <img src="${data.acProgress}">
+                          </div>
+
+                          <div>
+                              <span>Missing parts</span>
+                              <img src="${data.missingParts}">
+                          </div>
+                      </div>
+
+                      <div>
+                           <div>
+                              <span>Work Order Status</span>
+                              <img src="${data.workOrder}">
+                          </div>
+
+                          <div>
+                              <span>Efficiency</span>
+                              <img src="${data.efficiency}">
+                          </div>
+                       </div>
+
+                      <div>
+                          <div>
+                              <span>Open No Conformities & warnings</span>
+                              <img src="${data.noConforAndWarning}">
+                          </div>
+
+                          <div>
+                              <span>ERC & Remarks</span>
+                              <img src="${data.ercRemarks}">
+                          </div>
+                      </div>`
+
+      const modal2 = `
+                      <div>
+                          <div>
+                              <span>AC Progress - Station</span>
+                              <img src="${data.acProgress}">
+                          </div>
+                      </div>
+                      <div>
+                          <div>
+                              <span>Flight line - test</span>
+                              <img src="${data.fl_test}">
+                          </div>
+                      </div>
+
+                      <div>
+                          <div>
+                              <span>flights</span>
+                              <img src="${data.flights}">
+                          </div>
+
+                          <div>
+                              <span>hand over</span>
+                              <img src="${data.handOver}">
+                          </div>
+                      </div>
+
+                      <div>
+                          <div>
+                              <span>Open No Conformities & Remarks</span>
+                              <img src="${data.noConforAndRemarks}">
+                          </div>
+
+                          <div>
+                              <span>TBL</span>
+                              <img src="${data.tbl}">
+                          </div>
+                      </div>`
+
+      data?.delivery == false
+        ? main.innerHTML += modal1
+        : main.innerHTML += modal2;
+
+      modalContainer.appendChild(main)
+
+      planeModal.classList.add('modal--show');
+    })
+
+    cardContent.appendChild(plane_graph);
+    cardContent.innerHTML +=
+      `<article class="production__card_description">
+      </article>`
+
+    card.appendChild(cardContent);
+
+    cardSection.appendChild(card);
+
+  });
+}
